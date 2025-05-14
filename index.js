cad2f2bf981a05a0d44c16f5d7f3923b9c84626b
SHAGUN YADAV <shubham@Shaguns-MacBook-Air.local>
2025-05-14 14:57:02 +0530

    practice work

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/shagun', (req, res) => {
+  console.log('Shagun route accessed');
+  res.send('Hello ji twitter!');
+});
+
+app.get('/login', (req,res) => {
+    res.send('<h1>please loogin at chai or code</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
